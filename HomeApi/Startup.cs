b6df502c884ee508054f98aee8a929a00760a9a8
olegnizh <olegnizh@mail.ru>
2024-07-04 00:43:36 +0300

    inject of config

--- HomeApi/Startup.cs
+++ HomeApi/Startup.cs
@@ -1,3 +1,4 @@
+using HomeApi.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -15,21 +16,28 @@ namespace HomeApi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
 
-        public IConfiguration Configuration { get; }
+        /// <summary>
+        /// Загрузка конфигурации из файла Json
+        /// </summary>
+        private IConfiguration Configuration
+        { get; } = new ConfigurationBuilder()
+          .AddJsonFile("HomeOptions.json")
+          .Build();
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Добавляем новый сервис
+            services.Configure<HomeOptions>(Configuration);
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HomeApi", Version = "v1" });
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "HomeApi",
+                    Version = "v1"
+                });
             });
         }
 
