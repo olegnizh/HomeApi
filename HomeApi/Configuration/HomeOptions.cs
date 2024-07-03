7cedb0ac5a0be87ee3967d8579e621187a7fd0e8
olegnizh <olegnizh@mail.ru>
2024-07-03 12:23:04 +0300

    модель конфигурации

---
+++ HomeApi/Configuration/HomeOptions.cs
@@ -0,0 +1,17 @@
+﻿using HomeApi.Configuration;
+
+
+namespace HomeApi.Configuration
+{
+    public class HomeOptions
+    {
+        public int FloorAmount { get; set; }
+        public string Telephone { get; set; }
+        public Heating Heating { get; set; }
+        public int CurrentVolts { get; set; }
+        public bool GasConnected { get; set; }
+        public int Area { get; set; }
+        public Material Material { get; set; }
+        public Address Address { get; set; }
+    }
+}
