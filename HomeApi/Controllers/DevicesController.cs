dd04492f25c496abedaaaf3f1fb179febff6995a
olegnizh <olegnizh@mail.ru>
2024-07-06 14:07:06 +0300

    Devicess,  DevicesController - empty

---
+++ HomeApi/Controllers/DevicesController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using HomeApi.Configuration;
+using HomeApi.Contracts.Devices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace HomeApi.Controllers
+{
+    /// <summary>
+    /// Контроллер статусов устройств
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class DevicesController : ControllerBase
+    {
+        private IOptions<HomeOptions> _options;
+        private IMapper _mapper;
+
+        public DevicesController(IOptions<HomeOptions> options, IMapper mapper)
+        {
+            _options = options;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Просмотр списка подключенных устройств
+        /// </summary>
+        [HttpGet]
+        [Route("")]
+        public IActionResult Get()
+        {
+            return StatusCode(200, "Устройства отсутствуют");
+        }
+
+        /// <summary>
+        /// Добавление нового устройства
+        /// </summary>
+        [HttpPost]
+        [Route("Add")]
+        public IActionResult Add()
+             
+        {
+            return StatusCode(200, "Этот метод будет добавлять новые устройства");
+        }
+    }
+}
